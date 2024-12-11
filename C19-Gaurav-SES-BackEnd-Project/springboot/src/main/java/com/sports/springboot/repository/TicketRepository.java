27d9d2f6f7cc4db835cb2952ae6aeaa442867fd8
Gaurav T. Singh <gaurav.tsingh@ascendion.com>
2024-12-11 17:53:12 +0530

    Full Capstone

--- C19-Gaurav-SES-BackEnd-Project/springboot/src/main/java/com/sports/springboot/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sports.springboot.repository;
-
-import com.sports.springboot.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, String> {
-}
